d4bbd3e3dc7c9ff4d8ae5cd0d44fd874a424bf08
dxminikxx <dxminikxx@gmail.com>
2023-03-24 20:15:04 +0100

    1

--- app/src/main/java/com/example/mozeterazsieuda/MainActivity.kt
+++ app/src/main/java/com/example/mozeterazsieuda/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.mozeterazsieuda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnRegister: Button
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRegister = findViewById(R.id.btn_register)
+        btnLogin = findViewById(R.id.btn_login)
+
+        btnRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
