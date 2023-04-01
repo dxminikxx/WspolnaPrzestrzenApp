c3e40319f4260b16355716f3b0da533eda29fc32
Dominika Nowak <dxminikxx@gmail.com>
2023-04-01 18:27:36 +0200

    calendar changes

--- app/src/main/java/com/example/mozeterazsieuda/CalendarFragment.kt
+++ app/src/main/java/com/example/mozeterazsieuda/CalendarFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
+import com.example.mozeterazsieuda.DBHelper
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +20,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CalendarFragment : Fragment() {
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,13 +34,7 @@ class CalendarFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
-    }
+
 
     companion object {
         /**
@@ -56,4 +55,6 @@ class CalendarFragment : Fragment() {
                 }
             }
     }
+
+
 }
--- app/src/main/java/com/example/mozeterazsieuda/DBHelper.kt
+++ app/src/main/java/com/example/mozeterazsieuda/DBHelper.kt
@@ -1,17 +1,24 @@
 package com.example.mozeterazsieuda
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context):SQLiteOpenHelper(context, "Userdata", null, 1) {
+
+    private val TABLE_EVENTS = "EventLIST"
     override fun onCreate(p0: SQLiteDatabase?) {
         p0?.execSQL("create table Userdata (username TEXT primary key, password TEXT)")
+        //create event table
+        p0?.execSQL("create table $TABLE_EVENTS (id INTEGER primary key autoincrement, event_name TEXT, date TEXT, hour TEXT)")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         p0?.execSQL("drop table if exists Userdata ")
+        //drop event table
+        p0?.execSQL("drop table if exists $TABLE_EVENTS ")
     }
 
     fun instertdata(username: String, password: String): Boolean{
@@ -21,6 +28,20 @@ class DBHelper(context: Context):SQLiteOpenHelper(context, "Userdata", null, 1) 
         cv.put("password", password)
         val result = p0.insert("Userdata", null, cv)
         if (result==-1 .toLong()){
+            return false
+        }
+        return true
+    }
+
+    //method added information about new event
+    fun insterEventData(eventName: String, date: String, hour: String): Boolean{
+        val p0 = this.writableDatabase
+        val cv = ContentValues()
+        cv.put("event_name", eventName)
+        cv.put("date", date)
+        cv.put("hour", hour)
+        val result = p0.insert(TABLE_EVENTS, null, cv)
+        if (result == -1 .toLong()){
             return false
         }
         return true
@@ -37,4 +58,47 @@ class DBHelper(context: Context):SQLiteOpenHelper(context, "Userdata", null, 1) 
         cursor.close()
         return true
     }
+
+    //method downloader information about event from table
+    @SuppressLint("Range")
+    fun getEventData(): MutableList<Triple<String, String, String>>{
+        val p0 = this.readableDatabase
+        val eventList = mutableListOf<Triple<String, String, String>>()
+        val query = "select * from $TABLE_EVENTS"
+        val cursor = p0.rawQuery(query, null)
+        if(cursor.moveToFirst()){
+            do{
+                val eventName = cursor.getString(cursor.getColumnIndex("event_name"))
+                val date = cursor.getString(cursor.getColumnIndex("date"))
+                val hour = cursor.getString(cursor.getColumnIndex("hour"))
+                eventList.add(Triple(eventName, date, hour))
+            }while(cursor.moveToNext())
+        }
+        cursor.close()
+        return eventList
+    }
+
+    // metoda aktualizująca dane o wydarzeniach w tabeli
+    fun updateEventData(eventName: String, newDate: String, newHour: String): Boolean{
+        val p0 = this.writableDatabase
+        val cv = ContentValues()
+        cv.put("date", newDate)
+        cv.put("hour", newHour)
+        val result = p0.update(TABLE_EVENTS, cv, "event_name=?", arrayOf(eventName))
+        if(result == 0){
+            return false
+        }
+        return true
+    }
+
+    // metoda usuwająca dane o wydarzeniach z tabeli
+    fun deleteEventData(eventName: String): Boolean{
+        val p0 = this.writableDatabase
+        val result = p0.delete(TABLE_EVENTS, "event_name=?", arrayOf(eventName))
+        if(result == 0){
+            return false
+        }
+        return true
+    }
+
 }
