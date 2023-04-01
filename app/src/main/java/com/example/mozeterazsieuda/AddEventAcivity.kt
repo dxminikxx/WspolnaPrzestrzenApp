a09073d29b03f72f7d7168070afa6bafdb959c8d
Dominika Nowak <dxminikxx@gmail.com>
2023-04-01 18:38:37 +0200

    calendar changes

---
+++ app/src/main/java/com/example/mozeterazsieuda/AddEventAcivity.kt
@@ -0,0 +1,11 @@
+package com.example.mozeterazsieuda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEventAcivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_event_acivity)
+    }
+}
--- app/src/main/java/com/example/mozeterazsieuda/CalendarFragment.kt
+++ app/src/main/java/com/example/mozeterazsieuda/CalendarFragment.kt
@@ -1,10 +1,12 @@
 package com.example.mozeterazsieuda
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import com.example.mozeterazsieuda.DBHelper
@@ -21,40 +23,64 @@ private const val ARG_PARAM2 = "param2"
  */
 class CalendarFragment : Fragment() {
 
+    private lateinit var btnAddEvent: Button
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        btnAddEvent = view.findViewById(R.id.btn_addEvent)
+        btnAddEvent.setOnClickListener {
+            val intent = Intent(activity, AddEventAcivity::class.java)
+            startActivity(intent)
         }
+
+        return view
     }
 
-
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalendarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_calendar, container, false)
+//    }
+//
+//
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment CalendarFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            CalendarFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 
 
 }
